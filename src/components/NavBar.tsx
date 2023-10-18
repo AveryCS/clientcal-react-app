4f501135c10c6e7e5b682d251230404da07e31c2
AveryCS <averydcs@gmail.com>
2023-10-18 16:01:21 -0400

    NavBar in center, as desired

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,9 +6,11 @@ import './NavBar.css'; // Import your CSS file
 const NavBar: React.FC = () => {
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/create-report">Generate Report</Link>
-      <Link to="/add-client">Add New Client</Link>
+      <div className="navbar-links">
+        <Link to="/">Home</Link>
+        <Link to="/create-report">Generate Report</Link>
+        <Link to="/add-client">Add New Client</Link>
+      </div>
     </div>
   );
 };
