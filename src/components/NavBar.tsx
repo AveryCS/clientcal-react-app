02bbc810f4371e7974beb3521e8b8415967c06cc
AveryCS <averydcs@gmail.com>
2023-10-23 16:08:13 -0400

    Fixed navbar colors back to blue

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,56 @@
+// // NavBar.tsx
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import './css/NavBar.css';
+
+
+// const NavBar: React.FC = () => {
+//   return (
+//     <div className="navbar">
+//       <div className="navbar-links">
+//         <Link to="/">Home</Link>
+//         <Link to="/create-report">Generate Report</Link>
+//         <Link to="/add-client">Add New Client</Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NavBar;
+
+
 // NavBar.tsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './css/NavBar.css';
 
-
 const NavBar: React.FC = () => {
   return (
-    <div className="navbar">
-      <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/create-report">Generate Report</Link>
-        <Link to="/add-client">Add New Client</Link>
+    <nav className="navbar navbar-expand navbar-light bg-light style=#3498db">
+   
+      <Link className="navbar-brand" to="/">
+       
+      </Link>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+          <Link to="/" style={{ color: '#3498db' }}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+          <Link to="/create-report" style={{ color: '#3498db' }}>
+              Generate Report
+            </Link>
+          </li>
+          <li className="nav-item">
+          <Link to="/add-client" style={{ color: '#3498db' }}>
+              Add New Client
+            </Link>
+          </li>
+        </ul>
       </div>
-    </div>
+    </nav>
   );
 };
 
